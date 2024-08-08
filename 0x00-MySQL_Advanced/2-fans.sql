cc18ddcebb45f034a13b43aefef7cbfb0a1aaaf6
BrahimElhajji <elhajji080898@gmail.com>
2024-08-08 16:10:05 -0700

    done

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,3 @@
 -- Select the origin and sum of fans for each origin, aliasing the sum as nb_fans
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
