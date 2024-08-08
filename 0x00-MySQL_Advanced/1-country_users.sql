8a864c4ddb2891ea3f91b977254bac509c4cb0a8
BrahimElhajji <elhajji080898@gmail.com>
2024-08-08 10:58:22 -0700

    second task

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,17 @@
+-- Create the 'users' table only if it does not already exist
+CREATE TABLE IF NOT EXISTS users (
+    -- 'id' is an integer that cannot be null, auto-increments, and serves as the primary key
+    id INT NOT NULL AUTO_INCREMENT,
+    
+    -- 'email' is a string with a maximum length of 255 characters, cannot be null, and must be unique
+    email VARCHAR(255) NOT NULL UNIQUE,
+    
+    -- 'name' is a string with a maximum length of 255 characters, it can be null
+    name VARCHAR(255),
+
+    -- 'country' is an enumeration with possible values 'US', 'CO', and 'TN'. It cannot be null and defaults to 'US'
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    
+    -- Set 'id' as the primary key of the table
+    PRIMARY KEY (id)
+);
