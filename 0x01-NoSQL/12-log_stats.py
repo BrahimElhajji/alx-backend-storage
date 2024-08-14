2a1a760f0fee990c5d2d44c3e80e870a639e8c82
BrahimElhajji <elhajji080898@gmail.com>
2024-08-13 19:02:43 -0700

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Python script that provides some stats about Nginx logs stored in MongoDB"""
+
+def log_stats():
+    """
+    Provides stats about Nginx logs stored in MongoDB.
+    """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    nginx_collection = db.nginx
+
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
